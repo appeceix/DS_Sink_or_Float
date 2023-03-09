2fe79bfe41bdd9f1033d9698985ee6371e0462dc
Jose Prado <pradoappeceix@gmail.com>
2023-03-09 19:45:11 +0100

    Creación clase barco

---
+++ utils/clase_barco.py
@@ -0,0 +1,8 @@
+class Barco:
+    
+    def __init__(self, orientacion, origen, tamaño):
+        self.orientacion = orientacion
+        self.origen = origen
+        self.tamaño = tamaño
+    
+    
