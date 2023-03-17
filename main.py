833b4e6e02362e6ff313748106661b6a16f80c4a
Jose Prado <pradoappeceix@gmail.com>
2023-03-17 14:09:50 +0100

    Cambios para la versión casi definitiva.

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import numpy as pd
+# IMPORTACIÓN DE LIBRERÍAS #
+
+import numpy as np
 import pandas as pd
 import random
+import time
 import sys
 import os
-
 sys.path.append(os.getcwd())
-
 import pygame
 pygame.init()
 pygame.mixer.init()
 sound = pygame.mixer.Sound("sonidocanon.wav")
-soundwater = pygame.mixer.Sound("sonidoagua.wav")
+sonidoagua = pygame.mixer.Sound("sonidoagua.wav")
 soundwin = pygame.mixer.Sound("sonidowin.wav")
 soundlost = pygame.mixer.Sound("sonidolost.mp3")
 
@@ -25,25 +26,50 @@ from utils.config import BOARD_SIZE, MAR, BARCO, DISPARO_AGUA, DISPARO_BARCO,\
 from utils.funciones import colocar_barcos_jugador, colocar_barcos_maquina, disparo_jugador, disparo_maquina
       
 
-
 # DESARROLLO DEL JUEGO #
+
+print("╔══════════════════════════════╗")
+print("║                              ║")
+print("║    ¡Bienvenido a Hundir la   ║")
+print("║           Flota!             ║")
+print("║                              ║")
+print("╚══════════════════════════════╝")
+
 
 print("""\n\n¡Bienvenido al juego Hundir la Flota! El objetivo es hundir
 los barcos de tu oponente antes de que él hunda los tuyos. Coloca tus
-barcos estratégicamente y elige con cuidado dónde disparar. \n\n¡Que gane el mejor capitán! \n\n\n""")
+barcos estratégicamente y elige con cuidado dónde disparar. \n\n¡Que gane el mejor capitán! \n\n\n\
+Empecemos colocando tus barcos. \n\n""")
 
-colocar_barcos_jugador()
+time.sleep(2) # A lo largo del código hay varios time.sleep para que no sea todo tan instantáneo y mejore la experiencia.
 
-colocar_barcos_maquina()
+colocar_barcos_jugador() # En este punto del juego el jugador coloca sus barcos.
 
-while VIDAS_MAQUINA > 0 and VIDAS_JUGADOR > 0:
+time.sleep(3)
 
-    while VIDAS_MAQUINA > 0:
+colocar_barcos_maquina() # Acto seguido la máquina coloca sus posiciones automáticamente
+
+time.sleep(1)
+
+
+
+
+print("\n\n\n¡Ambos tableros han sido colocados! ¡¡¡A los cañones!!!\n\n\n")
+
+print("───────▓▓╬▓")
+print("──────▓▓▓║▓▓")
+print("─────▓▓▓▓╬▓▓▓▓")
+print("░░▄░▓▓▓▓▓║▓▓▓▓▓░░░░░")
+print("░▀████████████████▀░░\n\n\n")
+
+while VIDAS_MAQUINA > 0 and VIDAS_JUGADOR > 0: # Este es el mecanismo que hace que avancen los turnos mientras a algún jugador le quede vidas.
+
+    while VIDAS_MAQUINA > 0: # Esta es nuestra secuencia de disparo.
 
         disparo_jugador_x, disparo_jugador_y = disparo_jugador()
 
         if disparo_jugador_x < 0 or disparo_jugador_x >= BOARD_SIZE or \
-           disparo_jugador_y < 0 or disparo_jugador_y >= BOARD_SIZE:
+           disparo_jugador_y < 0 or disparo_jugador_y >= BOARD_SIZE: # Para corroborar las coordenadas del disparo.
             print("Coordenadas inválidas. Vuelve a intentarlo.")
             continue
 
@@ -62,28 +88,36 @@ while VIDAS_MAQUINA > 0 and VIDAS_JUGADOR > 0:
         if TABLERO1[disparo_jugador_x, disparo_jugador_y] == BARCO:
             TABLERO1[disparo_jugador_x, disparo_jugador_y] = DISPARO_BARCO
             VISOR[disparo_jugador_x, disparo_jugador_y] = DISPARO_BARCO # Para modificar nuestro mapa del enemigo con acierto.
-            VIDAS_MAQUINA -= 1
+            VIDAS_MAQUINA -= 1 # Restamos una vida a la máquina, avanzando en nuestro objetivo.
+            print("¡Proyectil lanzado! ¿Acertará..?")
+            time.sleep(2)
             print("¡Le has dado!")
             sound.play()
+            time.sleep(2)
             print(f"Has disparado en las siguientes posiciones\n{VISOR}") # Para ver en qué posiciones hemos disparado ya.
-            #print(tablero1)
+            time.sleep(2)
+            #print(tablero1) # Si queremos comprobar el tablero del enemigo podemos hacer este print.
 
-        
-        
         if TABLERO1[disparo_jugador_x, disparo_jugador_y] == MAR:
             TABLERO1[disparo_jugador_x, disparo_jugador_y] = DISPARO_AGUA
-            VISOR[disparo_jugador_x, disparo_jugador_y] = DISPARO_AGUA # Para modificar nuestro mapa del enemigo con agua.
+            VISOR[disparo_jugador_x, disparo_jugador_y] = DISPARO_AGUA # Para modificar nuestro visor del mapa del enemigo con agua.
+            print("¡Proyectil lanzado! ¿Acertará..?")
+            time.sleep(2)
             print("Disparo fallido. Cambio de turno")
-            soundwater.play()
+            sonidoagua.play()
+            time.sleep(2)
             #print(tablero1)
-            print(f"Has disparado en las siguientes posiciones\n{VISOR}") # Para ver en qué posiciones hemos disparado ya.
+            print(f"Has disparado en las siguientes posiciones\n{VISOR}")
             break
-        if VIDAS_MAQUINA == 0:
+
+        if VIDAS_MAQUINA == 0: # El objetivo del juego es llegar hasta este punto.
             print("¡Felicidades, has ganado!")
+            print("\n\n\nEsperamos que hayas disfrutado.\n\nIsmael Merino\nMatheus Zottis\n José Prado")
             soundwin.play()
             break
 
-    while VIDAS_JUGADOR > 0:
+
+    while VIDAS_JUGADOR > 0: # Esta sería la secuencia de disparo de la máquina.
 
         disparo_maquina_x, disparo_maquina_y = disparo_maquina()
 
@@ -97,22 +131,23 @@ while VIDAS_MAQUINA > 0 and VIDAS_JUGADOR > 0:
             print("La máquina ya ha disparado en esa casilla. Vuelve a intentarlo.")
             continue
 
-        if TABLERO[disparo_maquina_x, disparo_maquina_y] == BARCO:
+        if TABLERO[disparo_maquina_x, disparo_maquina_y] == BARCO: # Caso de acierto por parte de la máquina.
             TABLERO[disparo_maquina_x, disparo_maquina_y] = DISPARO_BARCO
-            VIDAS_JUGADOR -= 1
-            print("¡Le has dado!")
+            VIDAS_JUGADOR -= 1 # Cuando la máquina nos da a nosotros se nos resta una vida.
+            print("¡Ouch! ¡El enemigo te ha dado!")
             sound.play()
             print(TABLERO)
         
         
-        if TABLERO[disparo_maquina_x, disparo_maquina_y] == MAR:
+        if TABLERO[disparo_maquina_x, disparo_maquina_y] == MAR: # Caso de fallo por parte de la máquina.
             TABLERO[disparo_maquina_x, disparo_maquina_y] = DISPARO_AGUA
-            print("Disparo fallido. Cambio de turno")
-            soundwater()
+            print("El enemigo ha fallado su disparo. Ahora es tu turno.")
+            sonidoagua.play()
             print(TABLERO)
             break
 
-        if VIDAS_JUGADOR == 0:
+        if VIDAS_JUGADOR == 0: # Condición para que la máquina gane.
             soundlost.play()
             print("¡Lo siento, ha ganado la maquina! Intentalo de nuevo")
+            print("\n\n\nEsperamos que hayas disfrutado.\n\nIsmael Merino\nMatheus Zottis\n José Prado")
             break
--- utils/funciones.py
+++ utils/funciones.py
@@ -1,43 +1,51 @@
+# IMPORTACIÓN DE LIBRERÍAS #
+
 import numpy as np
 import pandas as pd
 import random
+import time
+import sys
+import os
 
-######CONFIGS#####
+sys.path.append(os.getcwd())
 
-BOARD_SIZE = 10
-MAR = "\u3030"
-BARCO ="\u26F5"
-DISPARO_AGUA = "\U0001F4A6"
-DISPARO_BARCO = "\U0001F4A5"
-TABLERO = np.full(fill_value=MAR, shape=(BOARD_SIZE, BOARD_SIZE))
-TABLERO1 = np.full(fill_value=MAR, shape=(BOARD_SIZE, BOARD_SIZE))
-VISOR = np.full(fill_value=MAR, shape=(BOARD_SIZE, BOARD_SIZE))
-BOAT_SIZE = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
-BOAT_SIZE1 = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4]
-CNT_BOAT= 0
-CNT_BOAT1 = 0
-VIDAS_JUGADOR = 20
-VIDAS_MAQUINA = 20
+import pygame
+pygame.init()
+pygame.mixer.init()
+sound = pygame.mixer.Sound("sonidocanon.wav")
+sonidoagua = pygame.mixer.Sound("sonidoagua.wav")
+soundwin = pygame.mixer.Sound("sonidowin.wav")
+soundlost = pygame.mixer.Sound("sonidolost.mp3")
+
+# IMPORTACIÓN DE VARIABLES CONFIG #
+
+from utils.config import BOARD_SIZE, MAR, BARCO, DISPARO_AGUA, DISPARO_BARCO,\
+      TABLERO, TABLERO1, VISOR, VIDAS_JUGADOR, VIDAS_MAQUINA
 
 
-##############################################1#################################
+# FUNCIONES #
+
 def colocar_barcos_jugador():
+    '''
+    Para colocar nuestros barcos en el tablero a través de tres inputs: 
+    orientación, coordenada X y coordenada Y.
+    '''
     from utils.config import CNT_BOAT, BOAT_SIZE
 
-    CNT_BOAT
-    while CNT_BOAT < 10:
 
-        for i in BOAT_SIZE: 
+    while CNT_BOAT < 10: # Para establecer el límite de barcos a colocar.
 
-            orient_valida = False
+        for i in BOAT_SIZE: # Este bucle cogerá el tamaño del barco de la lista de barcos.
+
+            orient_valida = False # De esta manera nos aseguramos que el input de orientación sea válido.
             while not orient_valida:
-                orient = input("Dame la orientacion del barco:  N, S, E o W: ")
+                orient = input("Dame la orientacion del barco (N, S, E o W): ")
                 if orient not in ["N", "S", "E", "W"]:
                     print("Orientación no válida, tiene que ser N, S, E o W")
                 else:
                     orient_valida = True
             
-            while True:
+            while True: # Comprobación de la validez de las coordenadas X e Y
                 try:
                     colocar_x = int(input("Dame la coordenada X: "))
                     if colocar_x < 0 or colocar_x >9:
@@ -57,70 +65,110 @@ def colocar_barcos_jugador():
                 except ValueError:
                     print("Coordenada Y no válida. Recuerda que debe ser entre 0 y 9. Inténtalo de nuevo:")
 
+
+
+
             if orient == "N":
+
                 if ((colocar_x-i+1) >= 0)&\
-                    (TABLERO[(colocar_x-i+1):(colocar_x+1), colocar_y] != BARCO).all():
+                    (TABLERO[(colocar_x-i+1):(colocar_x+1), colocar_y] != BARCO).all(): #Requisito para colocar un barco.
+
                     TABLERO[(colocar_x-i+1):(colocar_x+1), colocar_y] = BARCO
-                    CNT_BOAT += 1
-                    print(CNT_BOAT)
-                elif ((colocar_x-i+1) < 0):
+                    print(f"Barco número {CNT_BOAT+1} colocado:")
+                    CNT_BOAT += 1 # En caso True aumentamos el contador de barcos para cambiar el tamaño de los barcos.
+                    
+                elif ((colocar_x-i+1) < 0): # Advertencia
                     print("Te estás saliendo por arriba")
-                else:
-                    print("Ya hay un barco ahí")
-                print(TABLERO)            
+                   
+                else: # Advertencia
+                    print("¡Estás pisando otro barco!")
+
+                time.sleep(1)
+                print(TABLERO)
+
+                                
                 
             
             if orient == "S":
+
                 if ((colocar_x+i) <= BOARD_SIZE)&\
                     (TABLERO[colocar_x:(colocar_x+i), colocar_y] != BARCO).all():
+
                     TABLERO[colocar_x:(colocar_x+i), colocar_y] = BARCO
-                    CNT_BOAT += 1
-                    print(CNT_BOAT)
+                    print(f"Barco número {CNT_BOAT+1} colocado:")
+                    CNT_BOAT += 1 
+
                 elif ((colocar_x+i) > BOARD_SIZE):
                     print("Te estás saliendo por abajo")
+                    
                 else:
-                    print("Ya hay un barco ahí")
-                print(TABLERO)            
+                    print("¡Estás pisando otro barco!")
+                    
+                time.sleep(1)
+                print(TABLERO)   
+
+         
                     
             
             if orient == "E":
+                
                 if ((colocar_y+i) <= BOARD_SIZE)&\
                     (TABLERO[colocar_x, colocar_y:(colocar_y+i)] != BARCO).all():
+
                     TABLERO[colocar_x, colocar_y:(colocar_y+i)] = BARCO
+                    print(f"Barco número {CNT_BOAT+1} colocado:")
                     CNT_BOAT += 1
-                    print(CNT_BOAT)
+
                 elif ((colocar_y+i) > BOARD_SIZE):
                     print("Te estás saliendo por la derecha")
+                    
                 else:
-                    print("Ya hay un barco ahí")
-                print(TABLERO)            
+                    print("¡Estás pisando otro barco!")
+                    
+                time.sleep(1)
+                print(TABLERO)
+                
+
+             
                 
             
             if orient == "W":
+
                 if ((colocar_y-i+1) >= 0)&\
                     (TABLERO[colocar_x, (colocar_y-i+1):(colocar_y+1)] != BARCO).all():
+
                     TABLERO[colocar_x, (colocar_y-i+1):(colocar_y+1)] = BARCO
+                    print(f"Barco número {CNT_BOAT+1} colocado:")
                     CNT_BOAT += 1
-                    print(CNT_BOAT)
+
                 elif ((colocar_y-i+1) < 0):
                     print("Te estás saliendo por la izquierda")
+                    
                 else:
-                    print("Ya hay un barco ahí")
-                print(TABLERO)            
+                    print("¡Estás pisando otro barco!")
+                    
+                time.sleep(1)
+                print(TABLERO)     
+       
                 
-            if CNT_BOAT == 10:
-                print("\nTablero 1 con barcos colocados:")
+            if CNT_BOAT == 10: # Cuando llegamos a 10 se acaba el bucle se termina esta función.
+                print("\n Perfecto, ya has colocado todos tus barcos:")
+                time.sleep(1)
                 print(TABLERO)
                 break
-            
- ##############################################2#################################       
+                 
 
 
 
 def colocar_barcos_maquina():
-    from utils.config import CNT_BOAT1, BOAT_SIZE1
+    '''
+    Función que al llamarla colocará automáticamente los barcos\
+    de la máquina en su tablero. En principio nosotros no podemos verlo salvo que\
+    descomentemos el print oportuno.
+    '''
+    from utils.config import CNT_BOAT1, BOAT_SIZE1 
 
-    while CNT_BOAT1 < 10:
+    while CNT_BOAT1 < 11:
 
         letras_orient = ["N", "S", "E", "W"] # Lista para seleccionar orientación válida
         
@@ -140,16 +188,17 @@ def colocar_barcos_maquina():
                         (TABLERO1[(colocar_x-i+1):(colocar_x+1), colocar_y] != BARCO).all():
                         TABLERO1[(colocar_x-i+1):(colocar_x+1), colocar_y] = BARCO
                         colocado = True
-                        print(CNT_BOAT1)
-                        print(TABLERO1) 
+                        time.sleep(1)
+                        print(f"El enemigo ha colocado el barco número {CNT_BOAT1}.\n\n\n")
+                        # print(TABLERO1) 
 
-                    elif ((colocar_x-i+1) < 0):
-                        print("Te estás saliendo por arriba")
-
+                    elif ((colocar_x-i+1) < 0): # Supuestos de colocación incorrecta por parte de la máquina.
+                        # print("Te estás saliendo por arriba")
+                        pass
 
                     else:
-                        print("Ya hay un barco ahí")
-
+                        # print("Ya hay un barco ahí")
+                        pass
 
                 if orient == "S":
 
@@ -157,16 +206,17 @@ def colocar_barcos_maquina():
                         (TABLERO1[colocar_x:(colocar_x+i), colocar_y] != BARCO).all():
                         TABLERO1[colocar_x:(colocar_x+i), colocar_y] = BARCO
                         colocado = True
-                        print(CNT_BOAT1)
-                        print(TABLERO1) 
+                        time.sleep(1)
+                        print(f"El enemigo ha colocado el barco número {CNT_BOAT1}.\n\n\n")
+                        # print(TABLERO1) 
 
                     elif ((colocar_x+i) > BOARD_SIZE):
-                        print("Te estás saliendo por abajo")
-
+                        # print("Te estás saliendo por abajo")
+                        pass
 
                     else:
-                        print("Ya hay un barco ahí")
-
+                        # print("Ya hay un barco ahí")
+                        pass
 
                 if orient == "E":
 
@@ -174,16 +224,17 @@ def colocar_barcos_maquina():
                         (TABLERO1[colocar_x, colocar_y:(colocar_y+i)] != BARCO).all():
                         TABLERO1[colocar_x, colocar_y:(colocar_y+i)] = BARCO
                         colocado = True
-                        print(CNT_BOAT1)
-                        print(TABLERO1) 
+                        time.sleep(1)
+                        print(f"El enemigo ha colocado el barco número {CNT_BOAT1}.\n\n\n")
+                        # print(TABLERO1) 
 
                     elif ((colocar_y+i) > BOARD_SIZE):
-                        print("Te estás saliendo por la derecha")
-
+                        # print("Te estás saliendo por la derecha")
+                        pass
 
                     else:
-                        print("Ya hay un barco ahí")
-
+                        # print("Ya hay un barco ahí")
+                        pass
 
 
                 if orient == "W":
@@ -192,52 +243,61 @@ def colocar_barcos_maquina():
                         (TABLERO1[colocar_x, (colocar_y-i+1):(colocar_y+1)] != BARCO).all():
                         TABLERO1[colocar_x, (colocar_y-i+1):(colocar_y+1)] = BARCO
                         colocado = True
-                        print(CNT_BOAT1)
-                        print(TABLERO1) 
+                        time.sleep(1)
+                        print(f"El enemigo ha colocado el barco número {CNT_BOAT1}.\n\n\n")
+                        # print(TABLERO1) 
 
                     elif ((colocar_y-i+1) < 0):
-                        print("Te estás saliendo por la izquierda")
-
+                        # print("Te estás saliendo por la izquierda")
+                        pass
 
                     else:
-                        print("Ya hay un barco ahí")
-
+                        # print("Ya hay un barco ahí")
+                        pass
             CNT_BOAT1 += 1
-            if CNT_BOAT1 == 10:
-                print("\nTablero de la máquina con barcos colocados:")
-                print(TABLERO1)
-                break        
+            if CNT_BOAT1 == 11:
+                time.sleep(1)
+                print("\n\nEl enemigo ya ha colocado todos sus barcos\n\n")
+                # print(TABLERO1)
+                break         # Aquí termina la colocación de barcos de la máquina.
 
-############################################3##############################
+
+
 
 def disparo_jugador():
+    '''
+    Disparar en las coordenadas introducidas mediante input.
+    '''
     while True:
         while True:
             try:
-                disparo_jugador_x = int(input("Ingrese la coordenada x (0-9): "))
+                disparo_jugador_x = int(input("Ingrese la coordenada X (0-9): "))
                 if disparo_jugador_x < 0 or disparo_jugador_x > 9:
-                    print("Coordenada x fuera de rango. Intente de nuevo.")
+                    print("Coordenada X fuera de rango. Inténtalo de nuevo.")
                     continue
                 break
             except ValueError:
-                print("Coordenada x inválida. Intente de nuevo.")
+                print("Coordenada X inválida. Inténtalo de nuevo.")
 
         while True:
             try:
-                disparo_jugador_y = int(input("Ingrese la coordenada y (0-9): "))
+                disparo_jugador_y = int(input("Ingrese la coordenada Y (0-9): "))
                 if disparo_jugador_y < 0 or disparo_jugador_y > 9:
-                    print("Coordenada y fuera de rango. Intente de nuevo.")
+                    print("Coordenada Y fuera de rango. Inténtalo de nuevo.")
                     continue
                 break
             except ValueError:
-                print("Coordenada y inválida. Intente de nuevo.")
+                print("Coordenada Y inválida. Inténtalo de nuevo.")
                 
         return disparo_jugador_x, disparo_jugador_y
 
-############################################4##############################
+
 
 def disparo_maquina():
-        disparo_maquina_x = random.randint(0, 9)
-        disparo_maquina_y = random.randint(0, 9)
-        return (disparo_maquina_x, disparo_maquina_y)
+    '''
+    Función que hará que la máquina dispare automáticamente dentro de unos parámetros dados.
+    '''
+    disparo_maquina_x = random.randint(0, 9)
+    disparo_maquina_y = random.randint(0, 9)
+    return (disparo_maquina_x, disparo_maquina_y)
     
